e144c51b4a096eb11ff57e2d095b3815cdce76b7
MKGOLD <dkqk0124@naver.com>
2019-07-06 17:59:05 +0900

    [ADD] 이미지뷰 및 파이어베이스 연동 기능 추가, 사진과 사진 클릭 기능 필요.

--- app/src/main/java/com/example/duyustory/MainActivity.kt
+++ app/src/main/java/com/example/duyustory/MainActivity.kt
@@ -2,20 +2,47 @@ package com.example.duyustory
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mainAdapter : MainAdapter
-    private var catList = listOf<Cat>(Cat("a", "a"), Cat("b", "b"))
+    private lateinit var mainAdapter: MainAdapter
+    private var catList = arrayListOf<Cat>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val database = FirebaseDatabase.getInstance()
+        val usersDB = database.getReference("users")
+
         mainAdapter = MainAdapter(this, catList)
 
         duyu_recycler.setHasFixedSize(true)
         duyu_recycler.adapter = mainAdapter
+
+        usersDB.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (tempSnapshot in dataSnapshot.children) {
+                    val catData = tempSnapshot.getValue(Cat::class.java)
+                    catList.add(catData!!)
+                }
+                Log.d("catList", catList.toString())
+
+                mainAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(dataSnapshot: DatabaseError) {
+            }
+
+        })
+
+//        usersDB.setValue(catList)
+
     }
 }
--- app/src/main/java/com/example/duyustory/MainAdapter.kt
+++ app/src/main/java/com/example/duyustory/MainAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainAdapter(private val context: Context, private val catList: List<Cat>) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
@@ -21,10 +22,12 @@ class MainAdapter(private val context: Context, private val catList: List<Cat>) 
     inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val tvTitle = itemView.findViewById<TextView>(R.id.recycler_title)
         private val tvContent = itemView.findViewById<TextView>(R.id.recycler_content)
+        private val imageCatPicture = itemView.findViewById<ImageView>(R.id.recycler_catPicture)
 
         fun bind(catData : Cat) {
             tvTitle.text = catData.title
             tvContent.text = catData.contents
+            imageCatPicture.setImageResource(R.drawable.abc_ab_share_pack_mtrl_alpha)
         }
     }
 }
