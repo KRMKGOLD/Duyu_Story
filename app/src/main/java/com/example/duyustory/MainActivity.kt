26a934c088cb5cc0d32eb32c0f3f2bc8b17df36f
MKGOLD <dkqk0124@naver.com>
2019-07-09 19:26:51 +0900

    [ADD] MainActivity Menu 에 AddActivity 로 이동하는 버튼 제작.

--- app/src/main/java/com/example/duyustory/MainActivity.kt
+++ app/src/main/java/com/example/duyustory/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.duyustory
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -13,36 +18,55 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mainAdapter: MainAdapter
     private var catList = arrayListOf<Cat>()
+    private val database = FirebaseDatabase.getInstance()
+    private val usersDB = database.getReference("users")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val database = FirebaseDatabase.getInstance()
-        val usersDB = database.getReference("users")
 
         mainAdapter = MainAdapter(this, catList)
 
         duyu_recycler.setHasFixedSize(true)
         duyu_recycler.adapter = mainAdapter
 
+        progressBar.visibility = View.VISIBLE
+
+        getDataInDB()
+        // DB에서 데이터 받아오는 함수
+    }
+
+    private fun getDataInDB() {
         usersDB.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (tempSnapshot in dataSnapshot.children) {
                     val catData = tempSnapshot.getValue(Cat::class.java)
                     catList.add(catData!!)
                 }
-                Log.d("catList", catList.toString())
+                catList.reverse()
 
+                progressBar.visibility = View.GONE
                 mainAdapter.notifyDataSetChanged()
             }
 
             override fun onCancelled(dataSnapshot: DatabaseError) {
+                Toast.makeText(this@MainActivity, "취소되었습니다.", Toast.LENGTH_SHORT).show()
             }
-
         })
+    }
 
-//        usersDB.setValue(catList)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.startAddActivityMenu -> {
+                val startAddActivityIntent = Intent(this, AddActivity::class.java)
+                startActivity(startAddActivityIntent)
+            }
+        }
+        return true
     }
 }
