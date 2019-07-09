9c45115607cb9158be57a6e22c1898c49b333fc7
MKGOLD <dkqk0124@naver.com>
2019-07-09 23:43:48 +0900

    [ADD] AddActivity Design, ImageView Rounded Background

--- app/src/main/java/com/example/duyustory/AddActivity.kt
+++ app/src/main/java/com/example/duyustory/AddActivity.kt
@@ -2,11 +2,17 @@ package com.example.duyustory
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.drawable.GradientDrawable
+import kotlinx.android.synthetic.main.activity_add.*
 
 class AddActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        val drawable = this@AddActivity.getDrawable(R.drawable.background_rounding) as GradientDrawable
+        catImageView.background = drawable
+        catImageView.clipToOutline = true
     }
 }
--- app/src/main/java/com/example/duyustory/MainActivity.kt
+++ app/src/main/java/com/example/duyustory/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.duyustory
 
 import android.content.Intent
+import android.graphics.drawable.GradientDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
