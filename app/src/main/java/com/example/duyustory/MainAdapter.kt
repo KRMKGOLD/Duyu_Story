6230a11b45471346d5d0f6d285734188c4cc19fd
MKGOLD <dkqk0124@naver.com>
2019-07-26 23:57:02 +0900

    [ADD] PictureActivity 이동 추가, Glide를 통한 사진 보여주기 기능 필요.

--- app/src/main/java/com/example/duyustory/MainAdapter.kt
+++ app/src/main/java/com/example/duyustory/MainAdapter.kt
@@ -1,13 +1,13 @@
 package com.example.duyustory
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import com.bumptech.glide.Glide
 
 class MainAdapter(private val context: Context, private val catList: List<Cat>) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
@@ -21,8 +21,8 @@ class MainAdapter(private val context: Context, private val catList: List<Cat>) 
         holder.bind(catList[position])
     }
 
-    inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
-        private val tvTitle = itemView.findViewById<TextView>(R.id.recycler_title)
+    inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+                private val tvTitle = itemView.findViewById<TextView>(R.id.recycler_title)
         private val tvContent = itemView.findViewById<TextView>(R.id.recycler_content)
         private val imageCatPicture = itemView.findViewById<ImageView>(R.id.recycler_catPicture)
 
@@ -34,9 +34,14 @@ class MainAdapter(private val context: Context, private val catList: List<Cat>) 
             imageCatPicture.setOnClickListener(this)
         }
 
-        override fun onClick(v : View?) {
-            when (v?.id) {
-                // TODO : Image Data (imageCatPicture.Data) 를 Intent로 넘겨주고 PictureActivity 에서 띄워주기
+        override fun onClick(v: View?) {
+            when(v?.id) {
+                R.id.recycler_catPicture -> {
+                    val startPictureActivityIntent = Intent(context, PictureActivity::class.java)
+                    startPictureActivityIntent.putExtra("Picture_URL", catList[adapterPosition].image)
+
+                    context.startActivity(startPictureActivityIntent)
+                }
             }
         }
     }
--- app/src/main/java/com/example/duyustory/PictureActivity.kt
+++ app/src/main/java/com/example/duyustory/PictureActivity.kt
@@ -1,12 +1,19 @@
 package com.example.duyustory
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class PictureActivity : AppCompatActivity() {
+
+    private lateinit var image_URL : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture)
+
+        image_URL = intent.getStringExtra("Picture_URL")
+        Log.d("image_URL", image_URL)
     }
 }
