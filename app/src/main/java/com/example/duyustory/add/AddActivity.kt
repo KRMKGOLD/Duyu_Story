e794e713361d3b4188f3085e61d14f8adfcea634
MKGOLD <dkqk0124@naver.com>
2019-12-09 10:27:06 +0900

    [Add] TedRx2Permission을 이용한 권한 설정 로직 제작.

--- app/src/main/java/com/example/duyustory/add/AddActivity.kt
+++ app/src/main/java/com/example/duyustory/add/AddActivity.kt
@@ -1,5 +1,8 @@
 package com.example.duyustory.add
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
@@ -22,9 +25,12 @@ import kotlinx.android.synthetic.main.activity_add.*
 import java.io.ByteArrayOutputStream
 import java.util.*
 import android.view.View
+import androidx.loader.content.CursorLoader
 import com.example.duyustory.data.Cat
 import com.example.duyustory.R
 import com.example.duyustory.util.AddPictureUtil
+import com.gun0912.tedpermission.TedPermissionResult
+import com.tedpark.tedpermission.rx2.TedRx2Permission
 import java.lang.ref.WeakReference
 
 class AddActivity : AppCompatActivity() {
@@ -36,6 +42,11 @@ class AddActivity : AppCompatActivity() {
     private val usersDB = database.getReference("users")
     private var imageBitmap: Bitmap? = null
     private val addPictureUtil = AddPictureUtil()
+
+    override fun onStart() {
+        super.onStart()
+        getPermission()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,6 +94,8 @@ class AddActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
         if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.data != null) {
             imageBitmap = MediaStore.Images.Media.getBitmap(contentResolver, data.data)
 
@@ -98,22 +111,37 @@ class AddActivity : AppCompatActivity() {
         }
     }
 
+    private fun getPath(uri: Uri): String {
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursorLoader = CursorLoader(this, uri, projection, null, null, null)
+        val cursor = cursorLoader.loadInBackground()
+        val index = cursor?.getColumnIndexOrThrow(projection[0])
+
+        cursor?.moveToFirst()
+
+        return index?.let { cursor.getString(it) }!!
+    }
+
     private fun uploadCatImage() {
         val randomUUDI = "${UUID.randomUUID()}"
-        val storageImageRef = storageRef.child("image/$randomUUDI")
+        val storageImageRef = storageRef.child("imageUrl/$randomUUDI")
+
         val baos = ByteArrayOutputStream()
-        imageBitmap?.compress(Bitmap.CompressFormat.PNG, 100, baos)
         val catByteData = baos.toByteArray()
         val uploadTask = storageImageRef.putBytes(catByteData)
 
+        imageBitmap?.compress(Bitmap.CompressFormat.JPEG, 25, baos)
+        val a = imageBitmap
+
         uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> {
-            if (!it.isSuccessful) it.exception?.let { exception -> throw exception }
+            if (!it.isSuccessful) {
+                it.exception?.let { exception -> throw exception }
+            }
+
             return@Continuation storageImageRef.downloadUrl
         }).addOnCompleteListener {
             if (it.isSuccessful) {
-                pushCatDataInDB(
-                    Cat(it.result.toString())
-                )
+                pushCatDataInDB(Cat(it.result.toString()))
             } else {
                 Toast.makeText(this, "Error, DB(1) Error", Toast.LENGTH_SHORT).show()
             }
@@ -123,23 +151,42 @@ class AddActivity : AppCompatActivity() {
 
     private fun pushCatDataInDB(cat: Cat) {
         usersDB.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onCancelled(dataBaseError: DatabaseError) {
-                Toast.makeText(this@AddActivity, "취소되었습니다.", Toast.LENGTH_SHORT).show()
-            }
-
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 usersDB.push().setValue(cat)
                 Toast.makeText(this@AddActivity, "데이터를 저장했습니다.", Toast.LENGTH_SHORT).show()
                 addProgressBar.visibility = View.GONE
                 finish()
             }
+
+            override fun onCancelled(dataBaseError: DatabaseError) {
+                Toast.makeText(this@AddActivity, "취소되었습니다.", Toast.LENGTH_SHORT).show()
+            }
         })
     }
 
     private fun doRoundImageView() {
-        val drawable =
-            this@AddActivity.getDrawable(R.drawable.background_rounding) as GradientDrawable
+        val drawable = getDrawable(R.drawable.background_rounding) as GradientDrawable
         catImageButton.background = drawable
         catImageButton.clipToOutline = true
+    }
+
+    @SuppressLint("CheckResult")
+    private fun getPermission() {
+        TedRx2Permission.with(this)
+            .setRationaleTitle("권한 등록")
+            .setRationaleMessage("사진 등록을 위한 권한 설정이 필요합니다.")
+            .setDeniedMessage("사진 및 파일을 등록하기 위해서는 권한을 설정해주세요.")
+            .setPermissions(CAMERA, WRITE_EXTERNAL_STORAGE)
+            .request()
+            .subscribe({ result ->
+                if (result.isGranted) {
+                    Toast.makeText(this, "권한 승인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "권한 승인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            })
+
+
     }
 }
